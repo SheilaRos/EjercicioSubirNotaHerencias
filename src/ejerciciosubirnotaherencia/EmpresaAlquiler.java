e8cbc1b17318f69803ff80f76bf5ec5638af91ae
SheilaRos <sheila.ros.cheme@gmail.com>
2016-11-11 19:43:13 +0100

    Terminado

--- src/ejerciciosubirnotaherencia/EmpresaAlquiler.java
+++ src/ejerciciosubirnotaherencia/EmpresaAlquiler.java
@@ -1,5 +1,7 @@
 package ejerciciosubirnotaherencia;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,14 +17,41 @@ public class EmpresaAlquiler {
         misVehiculos.add(miMicrobus);
         misVehiculos.add(miFurgonetaCarga);
         misVehiculos.add(miCamion);
-        
-        for(Vehiculo v : misVehiculos){
-            try{
-                System.out.println("Introduce la matricula");
-                
-            }catch(Exception e){
-                System.out.println(e);
-            }
+        try{
+            boolean salir=false;
+            do{
+                BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+                System.out.println("Introduce la matricula de uno de nuestros vehículos:");
+                String matricula= br.readLine();
+            for(Vehiculo v : misVehiculos){
+                if(v.getMatricula().equalsIgnoreCase(matricula)){
+                    System.out.println("Introduce los días que desea utilizarlos:");
+                    int dias=Integer.parseInt(br.readLine());
+                    if(v instanceof Coches){
+                         Coches coche = (Coches) v;
+                        System.out.println(coche.precioTotal(dias));
+                    }
+                    else if(v instanceof Microbuses){
+                         Microbuses microbus = (Microbuses) v;
+                        System.out.println(microbus.precioTotal(dias));
+                    }
+                    else if(v instanceof Camiones){
+                         Camiones camion = (Camiones) v;
+                        System.out.println(camion.precioTotal(dias));
+                    }
+                    else if(v instanceof FurgonetaCarga){
+                        FurgonetaCarga furgo = (FurgonetaCarga) v;
+                        System.out.println(furgo.precioTotal(dias));
+                    }
+                    salir=true;
+                }        
+              }
+                if(!salir){
+                    System.out.println("No exite ningún vehículo con esta matricula en nuestra BBDD.");
+                }
+            }while(!salir);
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
     
